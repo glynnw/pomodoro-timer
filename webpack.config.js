12e7d16f9da09f826235ceb5771230b9bceb763a
Matthew Westbrook <horrorvacui@tutanota.com>
2017-05-20 16:06:06 -0500

    Convert entry.js from codepen to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = {
   entry: './src/entry.js',
@@ -10,18 +10,10 @@ module.exports = {
   module: {
     rules: [
       { test: /\.(js|jsx)$/, use: 'babel-loader'},
-      { test: /\.scss$/, use: extractSass.extract({
-        use: [{
-          loader: "css-loader"
-        }, {
-          loader: "sass-loader"
-        }],
-        fallback: "style-loader"
-      })}
+      { test: /\.scss$/, use: [ "style-loader", "css-loader", "sass-loader" ]}
     ]
   },
   plugins: [
-    extractSass,
     new HtmlWebpackPlugin({template: './src/index.html'})
   ],
   node: {
