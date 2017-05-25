37620e1ece14d6cd540ea97729f49bfd3664afdd
Matthew Westbrook <horrorvacui@tutanota.com>
2017-05-25 02:17:50 -0500

    Fix pausing a second late

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,5 +1,9 @@
 import { delay } from 'redux-saga'
-import { take, select, takeLatest, call, put, all } from 'redux-saga/effects'
+import {
+  cancelled, take, select,
+  takeLatest, call, put,
+  all, cancel, fork
+} from 'redux-saga/effects'
 import actions from '../actions/Actions'
 
 // selectors
@@ -7,42 +11,35 @@ const getElapsedTime = state => state.elapsedTime
 const getPomoLength = state => state.pomoLength
 
 export function* watchStartTimer() {
-  yield takeLatest('START_TIMER', tickAway)
-}
-
-export function* watchTick() {
-  yield takeLatest('TICK', tickAway)
-}
-
-export function* watchPauseTimer() {
   while(true) {
-    const { isStarted } = yield select()
-    yield take('PAUSE_TIMER')
-    console.log(isStarted)
+    yield take('START_TIMER')
+    const ticker = yield fork(tickAway)
+    yield take(['PAUSE_TIMER', 'TIMER_EXPIRE'])
+    yield cancel(ticker)
   }
 }
 
 export function* tickAway() {
-  let { isStarted, elapsedTime, timerLength } = yield select()
+  try {
+    while(true) {
+      const { elapsedTime, timerLength } = yield select()
 
-  console.log(isStarted)
-
-  if (isStarted) {
-    if (elapsedTime > timerLength * 60) {
-      yield put(actions.timerExpire())
-      // yield call(playTimerExpireSound)
+      if (elapsedTime > timerLength * 60) {
+        yield put(actions.timerExpire())
+        //  yield call(playTimerExpireSound)
+      }
+      yield call(delay, 1000)
+      yield put(actions.tick())
+      // yield call(playTickSound)
     }
-
-    yield call(delay, 1000)
-    yield put(actions.tick())
-    // yield call(playTickSound)
+  } finally{
+    if (yield cancelled())
+      console.log('pausing')
   }
 }
 
 export default function* rootSaga() {
   yield all([
-    watchTick(),
-    watchStartTimer(),
-    watchPauseTimer()
+    watchStartTimer()
   ])
 }
